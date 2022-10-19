571ce82b743fbcce304d285a685e2f3f122b9709
zzxjl1 <349888274@qq.com>
2022-10-19 21:43:40 +0800

    完善了注释

--- playground.py
+++ playground.py
@@ -9,10 +9,11 @@ from sensor import SUPPORTED_SAMPLE_TYPES
 
 SAMPLE_COUNT = 500
 x, y = generate_data(SAMPLE_COUNT)
-y = [i.index(1) for i in y]
+y = [i.index(1) for i in y]  # 将one-hot编码转换为索引
 
-"""
+
 def pca_1():
+    """PCA降维到1维"""
     global x, y
     pca = PCA(n_components=1)
     newX = pca.fit_transform(x)
@@ -23,10 +24,10 @@ def pca_1():
     plt.scatter(newX, newX, c=y)
     plt.title("pca_1")
     plt.show()
-"""
 
 
 def pca_2():
+    """PCA降维到2维"""
     global x, y
     pca = PCA(n_components=2)
     newX = pca.fit_transform(x)
@@ -40,6 +41,7 @@ def pca_2():
 
 
 def pca_3():
+    """PCA降维到3维"""
     global x, y
     pca = PCA(n_components=3)
     newX = pca.fit_transform(x)
@@ -55,6 +57,7 @@ def pca_3():
 
 
 def lda_2():
+    """LDA降维到2维"""
     global x, y
     clf = LinearDiscriminantAnalysis(n_components=2)
     newX = clf.fit_transform(x, y)
@@ -67,6 +70,7 @@ def lda_2():
 
 
 def lda_3():
+    """LDA降维到3维"""
     global x, y
     clf = LinearDiscriminantAnalysis(n_components=3)
     newX = clf.fit_transform(x, y)
@@ -81,6 +85,7 @@ def lda_3():
 
 
 def lda_predict():
+    """LDA预测"""
     global x, y
     clf = LinearDiscriminantAnalysis()
     clf.fit(x, y)
@@ -97,7 +102,7 @@ def lda_predict():
 
 
 if __name__ == "__main__":
-    # pca_1()
+    pca_1()
     pca_2()
     pca_3()
     lda_2()
